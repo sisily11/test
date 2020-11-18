36c756ef3da2df132b2223e4cf5d0427bd10caf9
DESKTOP-83DJ18L\ma <sisily11@gmail.com>
2020-11-18 13:18:27 +0900

    test

---
+++ android/app/src/main/kotlin/com/example/mari/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fultter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  final items = List.generate(100, (i) => i).toList();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ma Sang Sook'),
+      ),
+      body:
+      ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.event),
+            title: Text('Event'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.camera),
+            title: Text('Camera'),
+            trailing: Icon(Icons.navigate_next),
+            onTap: () {},
+          ),
+        ],
+      ),
+
+
+
+
+    );
+  }
+
+}
